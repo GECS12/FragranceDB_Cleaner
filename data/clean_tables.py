a7d105c66fcbad2547d5bed36ada7a24d4b078bb
GECS12 <traducao.gc@gmail.com>
2024-06-15 15:27:22 +0100

    Actualizaçoes

--- data/clean_tables.py
+++ data/clean_tables.py
@@ -1,7 +1,7 @@
 from db_utils import drop_table
 
 if __name__ == "__main__":
-    table_name = 'MassPerfumarias'
+    table_name = 'Perfumes24h'
     db_name = 'PT_fragrances'
 
     drop_table(db_name,table_name)
